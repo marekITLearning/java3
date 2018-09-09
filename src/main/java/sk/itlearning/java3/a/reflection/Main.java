e01a16d150cfb839b7280f2d1eecd8068cc82900
ladicky <ladicky@U510PL.itl.lan>
2018-09-09 09:36:55 +0200

    refl update

--- src/main/java/sk/itlearning/java3/a/reflection/Main.java
+++ src/main/java/sk/itlearning/java3/a/reflection/Main.java
@@ -2,6 +2,7 @@ package sk.itlearning.java3.a.reflection;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Main {
 
@@ -22,15 +23,19 @@ public class Main {
 				Class<Vypocet> cls = (Class<Vypocet>) Class.forName(config);
 				Constructor<Vypocet> con = cls.getConstructor(Integer.class);
 				v = con.newInstance(5);
+//				Method[] methods = cls.getMethods();
+//				for (Method m : methods) {
+//					System.out.println(m.getName());
+//					System.out.println(m.invoke(v));
+//				}
+				Method m = cls.getMethod("vypocitaj");
+				System.out.println(m.invoke(v));
 			} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException
 					| IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 					e.printStackTrace();
 			}
 
-			if (v != null) {
 
-				System.out.println(v.vypocitaj());
-			}
 		}
 
 	}
