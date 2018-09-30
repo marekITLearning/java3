f0e86bd5006c2b1fbc4706d81e7b1c04d79e4937
Marek Ladicky <marek.ladicky@gmail.com>
2018-09-30 23:20:25 +0200

    fix

--- java3/src/main/java/sk/itlearning/java3/o/clone/Main.java
+++ java3/src/main/java/sk/itlearning/java3/o/clone/Main.java
@@ -1,17 +1,20 @@
 package sk.itlearning.java3.o.clone;
 
+import java.math.BigDecimal;
+
 public class Main {
-	
+
 	public static void main(String[] args) {
-	
-//		Obcan o = new Obcan("1234", "Jozo").setKredit(200.0);
-//		
-//		System.out.println(o.getKredit());
-//		
+
+		Obcan o = new Obcan("1234", "Jozo");
+		o.setKredit(new BigDecimal(100));
+
+		System.out.println(o.getKredit());
+
 //		Obcan o2 = o.clone();
-//		
+//
 //		Obcan o3 = Obcan.clone(o2);
-		
+
 	}
-	
+
 }
