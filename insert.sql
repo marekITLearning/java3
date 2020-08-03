ad511c4c495717e0ed719f8c70813e71de53e463
java-semiconductor <java.semiconductor@gmail.com>
2020-08-03 19:18:38 +0200

    update

--- insert.sql
+++
@@ -1,6 +0,0 @@
-insert into Table (a, b, c) values (1, "a", "");
-insert into Table (a, b, c) values (2, "a", "");
-insert into Table (a, b, c) values (3, "a", "");
-insert into Table (a, b, c) values (4, "a", "");
-insert into Table (a, b, c) values (5, "a", "");
-insert into Table (a, b, c) values (6, "a", "");
